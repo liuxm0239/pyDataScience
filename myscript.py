529692db4a86c3a10af61d730c6b5a856280b68f
刘兴民(Xingmin Liu) <liuxingmin@mgi-tech.com>
2024-03-04 17:57:55 +0800

    Update ch02

--- myscript.py
+++ myscript.py
@@ -1,7 +1,32 @@
 def f(x, y, z):
+    """
+    test intro
+    """
     return (x + y) / z
 a = 5
 b = 6
 c = 7.5
+
+
+def dn(a, b):
+    """
+    Divide two numbers.
+ 
+    Parameters
+    ----------
+    a : float
+        The dividend.
+    b : float
+        The divisor.
+ 
+    Returns
+    -------
+    float
+        The quotient of the division.
+    """
+    if b == 0:
+        raise ValueError("Division by zero is not allowed.")
+    return a / b
+print(dn(3,6))
 
 result = f(a, b, c)
